c21b34d06a1538fc35090ab6d7a2d718091ee1d6
Gilles Sabathe <gilles.sabathe@free.fr>
2018-05-07 21:31:02 -0400

    File creation

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * Front controller
+ *
+ * -Application.php initialisation
+ * -Class autoload
+ * - find controller for a client request
+ */
+
+include_once('./_config.php');
+/* initialisation des fichiers TWIG */
+/*require_once 'vendor/twig/twig/lib/Twig/autoloader.php';*/
+require_once 'vendor/autoload.php';
+/** Autoload */
+MyAutoload::start();
+
+$request = $_GET['r'];
+
+if ($request == "")  {
+    $request = "home.html";
+}else {
+    $request = $_GET['r'] ;
+
+}
+// Routeur
+$routeur = new Routeur($request);
+$routeur ->findController();
