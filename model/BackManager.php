cee8b02583a33d23a0793e969db8c38b14be575b
Gilles Sabathe <gilles.sabathe@free.fr>
2018-05-07 23:55:04 -0400

    File creation

---
+++ model/BackManager.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Gilles
+ * Date: 07/05/2018
+ * Time: 22:22
+ */
+
+namespace Projet5;
+
+/**
+ * Class BackManager
+ * @package Projet5
+ *
+ * Back manager define method used in several managers
+ */
+class BackManager extends lib
+{
+    private  $bdd;
+    Public function bddAssign() {
+        try {
+            return $this->bdd = new PDO("mysql:host=".BDDDIR.";dbname=".DBNAME.";charset=utf8", USERNAME, PSWDB);
+        }catch(Exception $e){
+            die('Erreur : '.$e->getMessage());
+        }
+    }
+
+
+}
