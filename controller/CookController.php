0fb30895b62e4da7e32ccd102a16923dc67f3121
Gilles Sabathe <gilles.sabathe@free.fr>
2018-05-12 17:22:23 -0400

    Creations files

---
+++ controller/CookController.php
@@ -0,0 +1,121 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Gilles
+ * Date: 10/05/2018
+ * Time: 09:40
+ */
+
+class CookController extends lib
+{
+
+    /**
+     *
+     * showDishes()
+     *
+     * call manager to get all chapters in database .
+     * call a manger to get warning List on comment .
+     *
+     *
+     *
+     */
+
+    public function showHome()
+    {
+        //$this->sessionStatus();//determine status admin or not
+        // Call of manager to get all Chapters in DB
+        //$manager = new PostManager();
+       // $chapters= $manager->findAll();
+        // call of manager to get all warningList ( items and reply comment signaled by user)
+       // $warningListManager = new PostManager();
+       // $warningList= $warningListManager->getWarnings();
+
+/*
+        if (empty($warningList)){
+            // call of view in case of no warnings on comments and topic
+            $myView = new View('home');
+            $myView->build( array('chapters'=> $chapters ,'comments'=>null,'warningList' => null ,'HOST'=>HOST ,'adminLevel'=> $_SESSION['adminLevel']));
+
+        } else{
+            // call of view in case of warnings on comments and topic
+            $myView = new View('home');
+            $myView->build( array('chapters'=> $chapters ,'comments'=>null,'warningList' => $warningList,'HOST'=>HOST ,'adminLevel'=> $_SESSION['adminLevel']));
+        }
+*/
+        // Call of manager to get all recipes
+        $manager = new CookManager();
+        $recipes= $manager->findFeaturedDishes();
+        if (empty($recipes)){
+            $recipes= $manager->findDishes();
+        }
+        $_SESSION['adminLevel']=0;
+        $myView = new View('home');
+        $myView->build( array('recipes'=> $recipes ,'comments'=>null,'warningList' => null ,'HOST'=>HOST ,'adminLevel'=> $_SESSION['adminLevel']));
+    }
+
+    /**
+     * showDish()
+     *
+     * show one dish from disId
+     */
+
+    public function showDish()
+    {
+        // Call of manager to get all recipes
+
+        if (isset($_GET['dishId']) AND ($_GET['dishId'] >0) ) {
+            $manager = new CookManager();
+            $recipe= $manager->findDish($_GET['dishId']);
+            if (!empty($recipe)){
+                $_SESSION['adminLevel']=0;
+                $myView = new View('recipe');
+                $myView->build( array('recipes'=> $recipe ,'comments'=>null,'warningList' => null ,'HOST'=>HOST ,'adminLevel'=> $_SESSION['adminLevel']));
+            }else{
+                $_SESSION['adminLevel']=0;
+                $myView = new View('error');
+                $myView->build( array('recipes'=>null ,'comments'=>null,'warningList' => null ,'HOST'=>HOST ,'adminLevel'=> $_SESSION['adminLevel']));
+            }
+        }else {
+            $_SESSION['adminLevel']=0;
+        $myView = new View('error');
+        $myView->build( array('recipes'=>null ,'comments'=>null,'warningList' => null ,'HOST'=>HOST ,'adminLevel'=> $_SESSION['adminLevel']));
+        }
+
+    }
+
+    /**
+     * showCategory()
+     * .
+     * call manager to get recipe thank to a category
+     *
+     *
+     *
+     */
+    public function showCategory()
+    {
+        if (isset($_GET['category']) and $_GET['category']>0){
+            $manager = new CookManager();
+            $recipes= $manager->findCategory($_GET['category']);
+
+            if (!empty($recipes)){
+                $_SESSION['adminLevel']=0;
+                $myView = new View('home');
+                $myView->build( array('recipes'=> $recipes ,'comments'=>null,'warningList' => null ,'HOST'=>HOST ,'adminLevel'=> $_SESSION['adminLevel']));
+            }else{
+                $manager = new CookManager();
+                $recipes= $manager->findDishes();
+                $_SESSION['adminLevel']=0;
+                $myView = new View('home');
+                $myView->build( array('recipes'=> $recipes ,'comments'=>null,'warningList' => null ,'HOST'=>HOST ,'adminLevel'=> $_SESSION['adminLevel']));
+            }
+        }else {
+            $_SESSION['adminLevel']=0;
+            $myView = new View('error');
+            $myView->build( array('recipes'=>null ,'comments'=>null,'warningList' => null ,'HOST'=>HOST ,'adminLevel'=> $_SESSION['adminLevel']));
+        }
+
+    }
+
+
+
+}
---
+++ lib/View.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ *
+ * Class View
+ *
+ * define a View class
+ */
+
+class View {
+    private $template;
+
+    public function __construct($template)
+    {
+        $this -> template =$template;
+    }
+
+    public function build($datas)
+    {
+        $template = $this->template;
+
+        $loader = new Twig_Loader_Filesystem(ROOT . 'view/');
+
+        $twig = new Twig_Environment($loader, [
+            'cache' => false //__DIR__ .'/tmp'
+        ]);
+
+
+        if ($datas){
+            foreach ($datas as $name => $value) {
+                $a = $name;
+                $$a = $value;
+            }
+           // echo $twig->render($template . '.twig', ['chapters' => $chapters,'comments' => $comments ,'warningList' => $warningList,'HOST'=>$HOST, 'adminLevel' => $adminLevel]);
+           echo $twig->render($template . '.twig', ['recipes' => $recipes,'comments' => $comments ,'warningList' => $warningList,'HOST'=>$HOST, 'adminLevel' => $adminLevel]);
+        }
+    }
+
+    public function redirect($route)
+        {
+            header("location: ".HOST.$route );
+            exit;
+        }
+}
