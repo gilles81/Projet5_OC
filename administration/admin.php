f83cb01f0c412ba967d5939cbe946d22a7d6a3ea
Gilles Sabathe <gilles.sabathe@free.fr>
2018-05-12 17:08:22 -0400

    namespace addition

--- administration/admin.php
+++ administration/admin.php
@@ -1,11 +1,12 @@
 <?php
-define('DBNAME','Cooking');
+namespace Projet5;
+define('DBNAME','cooking');
 define('BDDDIR','localhost');
 define('USERNAME','root');
 define('PSWDB','');
 /**
 define('BLOGNAME','db736607164');
-define('BDDDIR','/db736607164.db.1and1.com');
+define('BDDDIR','db736607164.db.1and1.com');
 define('USERNAME','dbo736607164');
 define('PSWDB','JForteroche1234.');
  **/
