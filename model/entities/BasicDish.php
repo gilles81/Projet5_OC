dee4c38a58124ff66fdc652e868b738e3fdbcfa1
Gilles Sabathe <gilles.sabathe@free.fr>
2018-05-16 16:55:41 -0400

    add status attribute and hydrate fonction

--- model/entities/BasicDish.php
+++ model/entities/BasicDish.php
@@ -22,6 +22,54 @@ class BasicDish
     private $ingredients;
     private $difficulty;
     private $featured;
+    private $status;// R = Ready to published , D = Draft , W = Wainting list
+
+
+    public function __construct($data)
+    {
+        $this->hydrate($data);
+    }
+
+
+    public function hydrate(array $donnees)
+    {
+        foreach ($donnees as $key => $value)
+        {
+            // On récupère le nom du setter correspondant à l'attribut.
+            $method = 'set'.ucfirst($key);
+
+            // Si le setter correspondant existe.
+            if (method_exists($this, $method))
+            {
+                // On appelle le setter.
+                $this->$method($value);
+            }
+        }
+    }
+
+// …
+
+
+
+
+
+
+
+    /**
+     * @return mixed
+     */
+    public function getStatus()
+    {
+        return $this->status;
+    }
+
+    /**
+     * @param mixed $status
+     */
+    public function setStatus($status)
+    {
+        $this->status = $status;
+    }
 
     /**
      * @return mixed
