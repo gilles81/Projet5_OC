eaf623fd8025fe2eb5d9e4f4368e39df0dc67a31
Gilles Sabathe <gilles.sabathe@free.fr>
2018-05-16 17:06:54 -0400

    deletion of console.log (debug)

--- assets/js/Modal_remove.js
+++ assets/js/Modal_remove.js
@@ -1,6 +1,6 @@
 $(function() {
     $('#bt_eraser_receipe').click(function() {
-        console.log('je suis bien dans le JS de modalTest');
+
         $(".bd-eraserReceipe-modal-lg").modal('show');
     });
 });
