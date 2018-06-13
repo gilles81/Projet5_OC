a2bd7e16ac96812d6255f4689d690d734bb24433
Gilles Sabathe <gilles.sabathe@free.fr>
2018-06-12 20:25:18 -0400

    modification for ingredients list

--- assets/js/Ingredients.js
+++ assets/js/Ingredients.js
@@ -1,10 +1,36 @@
-/* Java script for */
+/* Java script for ingredients list in select2*/
+
 
 $(function() {
-    $(document).ready(function() {
+    $('#NewRecipeIngredient').select2(
+        {
+            placeholder: 'Selection d\'un ingredient',
 
-        $('#NewRecipeIngredient').select2({ placeholder: "Select ingredient",
-            allowClear: true  });
-
-    });
+        });
+/** this part is for a next developppemnt
+ *  it will be used to select 2 List ingredient
+ */
+/*
+    $('#NewRecipeIngredient').select2(
+        {
+           // placeholder: 'Selection d\'un ingredient',
+            ajax: {
+                type: 'post',
+                url: 'adminSearchComputeList.html',
+                data :{'dishId': $('#NewRecipeIngredientIdHidden').val()},
+                dataType: 'json'
+            },
+            processResults: function (data) {
+                return {
+                    results: data.items
+                };
+            }
+        });
+*/
 });
+
+
+
+
+
+
